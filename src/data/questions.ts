c19e9dc10aa5758bbb6199e1d644e65a016fffa9
Deeparishi <130769314+deeparishi@users.noreply.github.com>
2025-11-23 12:10:12 +0530

    Update questions to reflect personal memories

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -7,27 +7,63 @@ export interface Question {
 
 export const questions: Question[] = [
   {
-    question: "What was the first movie we watched together?",
+    question: "Deepu plan pani failure aana first movie name enna ammu?",
     type: "single",
-    options: ["The Notebook", "Titanic", "La La Land", "A Walk to Remember"],
-    answer: "The Notebook"
+    options: ["Love Today", "Kaathuvakula Rendu Kaadhal", "Sita Raman", "Vikram"],
+    answer: "Sita Raman"
   },
   {
-    question: "Which places have we traveled to together? (Select all)",
-    type: "multi",
-    options: ["Paris", "Tokyo", "New York", "London"],
-    answer: ["Paris", "Tokyo"]
-  },
-  {
-    question: "Where did we have our first date?",
+    question: "Our First Movie?",
     type: "single",
-    options: ["Coffee Shop", "Restaurant", "Park", "Beach"],
-    answer: "Coffee Shop"
+    options: ["Vikram", "Good Night", "Leo", "Chithha"],
+    answer: "Good Night"
   },
   {
-    question: "What are my favorite colors? (Select all)",
+    question: "2024 Anniversary ku enga ponom namba? ",
+    type: "single",
+    options: ["Vadapalani Kovil + Forum Mall", "Mylaport Kovil + Birla Planetarium", "Vadapalani Kovil + T Nagar National Park", "Marina Beach + EA Mall"],
+    answer: "Mylaport Kovil + Birla Planetarium"
+  },
+  {
+    question: "2024 Anniversary ku ena outfit potrunthom?",
+    type: "single",
+    options: ["Pink dress + Blue shirt", "Red chudi + Purple dress", "Sandal Shirt + Green chudi", "Blue Tshirt + Yellow dress"],
+    answer: "Red chudi + Purple dress"
+  },
+  {
+    question: "Rendu Perukum pedicha dishes",
     type: "multi",
-    options: ["Pink", "Blue", "Purple", "Green"],
-    answer: ["Pink", "Blue"]
+    options: ["Waffle", "Butterscotch Ice", "Rose milk", "Hakka Noodles", "Coffe", "Biriyani", "Home foods"],
+    answer: ["Waffle", "Butterscotch Ice", "Home foods", "Biriyani"]
+  },
+  {
+    question: "Which Dress la deepu ku Ammu va pedium",
+    type: "single",
+    options: ["Red Dress/White shall", "Purple Dress/Sandal Shall", "Pink Dress/Jeans", "Full White"],
+    answer: "Purple Dress/Sandal Shall"
+  },
+  {
+    question: "Proposal Date",
+    type: "single",
+    options: ["27/2/2022", "29/2/2023", "28/2/2023", "24/2/2023"],
+    answer: "28/2/2023"
+  },
+   {
+    question: "First time endha bike la enoda vantha",
+    type: "single",
+    options: ["Scooty", "Bike"],
+    answer: "Bike"
+  },
+  {
+    question: "Our First hug happened in which month?",
+    type: "single",
+    options: ["Sep", "Aug", "Oct", "Nov"],
+    answer: "Aug"
+  },
+   {
+    question: "Our First kiss?",
+    type: "single",
+    options: ["SIDCO", "Bike travel", "While movie", "At home"],
+    answer: "SIDCO"
   }
 ];
